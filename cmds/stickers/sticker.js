052f87097967cf3d84e7a316377ba5d5248dbf9d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-05-06 10:42:22 -0400

    Actualizar sticker.js

--- cmds/stickers/sticker.js
+++ cmds/stickers/sticker.js
@@ -21,7 +21,7 @@ export default {
       const name = user.name;
       const meta1 = user.metadatos ? String(user.metadatos).trim() : '';
       const meta2 = user.metadatos2 ? String(user.metadatos2).trim() : '';
-      let texto1 = meta1 ? meta1 : `ʏᴜᴋɪ 🧠 Wᴀʙᴏᴛ`;
+      let texto1 = meta1 ? meta1 : `sumi 🌤️ ʙᴏᴛ`;
       let texto2 = meta1 ? (meta2 ? meta2 : '') : `@${name}`;
       let urlArg = null;
       let argsWithoutUrl = [];
